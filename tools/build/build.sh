31bd6ff808000985ed373c6d05acc7e2b6db28bb
Blair Learn <blairlearn@users.noreply.github.com>
2017-03-22 11:59:19 -0400

    Store images in NCI-internal registry.

--- tools/build/build.sh
+++ tools/build/build.sh
@@ -6,14 +6,15 @@ export SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PROJECT_HOME="$(cd $SCRIPT_PATH/../.. && pwd)"
 export CURDIR=`pwd`
 
-export BASENAME=nciwebcomm/dotnet-docker
+export BASENAME=${DOCKER_REGISTRY}/ocpl/dotnet-docker
 
 # Do we have credentials to push the result?
-if [ "$DOCKER_USERNAME" == "" ]; then echo DOCKER_USERNAME not set; exit 1; fi
-if [ "$DOCKER_PASSWORD" == "" ]; then echo DOCKER_PASSWORD not set; exit 1; fi
+if [ -z "$DOCKER_USERNAME" ]; then echo DOCKER_USERNAME not set; exit 1; fi
+if [ -z "$DOCKER_PASSWORD" ]; then echo DOCKER_PASSWORD not set; exit 1; fi
+if [ -z "$DOCKER_REGISTRY" ]; then echo DOCKER_REGISTRY not set; exit 1; fi
 
 #
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD $DOCKER_REGISTRY
 
 
 builds=( "sdk" "runtime")
